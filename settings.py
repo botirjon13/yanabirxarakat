822bd3427f0d3c708b6148fcddcc6310acf011c3
botirjon13 <tomama-uz@mail.ru>
2025-10-08 14:51:38 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,21 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'core',  # bu joyda
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'core' / 'templates'],
+        'APP_DIRS': True,
+        ...
+    },
+]
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
